c81659f4a7ac63f0279d47a3cbda5d6d4f7d093c
maiga28 <sidimaiga.work@gmail.com>
2026-05-12 15:24:20 +0000

    adding admin create file2

--- create_admin.py
+++ create_admin.py
@@ -1,5 +1,23 @@
 import os
+import django
+
+# 1. On indique à Django où se trouvent les réglages
+# Remplace 'ton_projet' par le nom du dossier qui contient ton fichier settings.py
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'adminstudent@gmail.com', 'adminstudentmaiga1234')
+username = 'admin'
+email = 'adminstudent@gmail.com'
+password = 'adminstudentmaiga1234'
+
+if not User.objects.filter(username=username).exists():
+    print("Création du superutilisateur...")
+    User.objects.create_superuser(username, email, password)
+else:
+    print("Le superutilisateur existe déjà.")
+
+
+
